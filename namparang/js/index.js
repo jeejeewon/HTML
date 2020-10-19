e5af6891e178ed6f2fb09a39aa7572dbd4ca55d1
jeejeewon <68887960+jeejeewon@users.noreply.github.com>
2020-10-19 09:35:51 +0900

    Update

--- namparang/js/index.js
+++ namparang/js/index.js
@@ -9,10 +9,55 @@ $(function() {
 
 	resize();	
 	nav();
-
 	window.onresize = function() {
 		resize()		
 	};
+
+	$("#title>span").each(function(i) {	
+	    var easeInOutBack = $(this).attr("data-easeInOutBack");
+		$(this).stop().animate({
+			top: '0',
+			opacity: '1'
+		}, 100 * (i + 0.1),easeInOutBack);
+	});
+
+   $(window).scroll(function(e) {           
+        console.log($(window).scrollTop());   
+        showTitle()             
+    });
+
+
+/*swiper*/
+	var swiper1 = new Swiper('#view-1-swiper', {
+	 	allowTouchMove: true,
+		slidesPerView: 1,
+		spaceBetween: 30,
+		speed: 300,				
+		navigation: {
+		nextEl: '#view-1-swiper>.swiper-button-next',
+		prevEl: '#view-1-swiper>.swiper-button-prev',
+		}
+    });
+    var swiper2 = new Swiper('#view-2-swiper', {
+	 	allowTouchMove: true,
+		slidesPerView: 1,
+		spaceBetween: 30,
+		speed: 300,				
+		navigation: {
+		nextEl: '#view-2-swiper>.swiper-button-next',
+		prevEl: '#view-2-swiper>.swiper-button-prev',
+		}
+    });
+    var swiper3 = new Swiper('#view-3-swiper', {
+	 	allowTouchMove: true,
+		slidesPerView: 1,
+		spaceBetween: 30,
+		speed: 300,				
+		navigation: {
+		nextEl: '#view-3-swiper>.swiper-button-next',
+		prevEl: '#view-3-swiper>.swiper-button-prev',
+		}
+    });   
 
 
 });
@@ -64,4 +109,23 @@ function nav() {
 		$("#ticket-close").fadeOut(1100);
 	});
 
+};
+
+function showTitle() {
+	if ($(window).scrollTop() >= 1079) {
+		var easeInQuart = $(this).attr("data-easeInQuart");
+		$("#info>div:nth-child(1)>span").each(function(i) {		    
+			$(this).stop().animate({				
+				height: '60px'
+			}, 100 * (i + 0.1),easeInQuart);
+		});
+		setTimeout(function() {
+			$("#info>div:nth-child(1)>span>p").each(function(i) {		
+				$(this).stop().animate({	
+					opacity: '1',
+					top: '0'
+				}, 100 * (i + 0.1),easeInQuart);					
+			});	
+		},800); clearTimeout();	
+	};
 };
