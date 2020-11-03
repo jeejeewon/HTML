c9f2027310c49c71c22674d998bc2053b5c14b0b
jeejeewon <68887960+jeejeewon@users.noreply.github.com>
2020-11-03 11:37:28 +0900

    Update

--- travel/js/index.js
+++ travel/js/index.js
@@ -5,30 +5,22 @@ $(function() {
 	  window.scrollTo(0, 0);
 	}
 	loading();
-	resize();	
-	// scrollUpDown();	
-	nav();
+	resize();		
 	hoverMenu();
-	window.onresize = function() {
-		resize();				
-	};
 
-	/*burger nav*/
-	$("#burger-wrap").click(function(){		
-		$("#top-nav-wrap").animate({
-			top: '0'
-		},600);
-		$("#top-nav-wrap").mouseleave(function(){
-			$("#top-nav-wrap").animate({
-				top: '-100px'
-			},600);
-		});		 
-	});
+	window.onresize = function() {
+		resize();		
+		BurgerNav();
+		$("#loading-logo").center();
+	};
+	BurgerNav();
+	hoverBox();	
+	
 	$("#bottom-nav").click(function(){		
 		$("#bottom-nav-wrap").animate({
 			bottom: '0'
-		},600,'easeOutElastic');
-	});
+		},300,'easeOutElastic');
+	});			
 
 	/*bottom active*/	
 	$("#bottom-bar").mouseleave(function(){
@@ -44,8 +36,7 @@ $(function() {
 			console.log($(window).scrollTop());
 			// if($(window).scrollTop() + $(window).height() == $(document).height()) { alert("bottom!"); } //바닥확인		
 
-			// showContents();	
-			hoverBox();		
+			// showContents();					
 
 			/*side nav*/			
 			if ($(window).scrollTop() >= 800) {
@@ -60,12 +51,6 @@ $(function() {
 				},800,'easeInQuad');			
 			};
 			/*bottom nav*/			
-			// if ($(window).scrollTop() < 900) {
-			// 	$("#bottom-nav-wrap").stop().animate({			
-			// 		bottom: '-460px',						
-			// 	},300,'easeInQuad');	
-			// } 
-			// else
 			if ($(window).scrollTop() < 4040) {		
 				$("#bottom-nav-wrap").stop().animate({				
 					bottom: '-360px'						
@@ -87,11 +72,30 @@ $(function() {
 			}; 							        
 		});			
 	},4000); 
+
+	$("#to-top").click(function(){
+		goto("#top");
+		console.log(goto())
+	});
+	$("#scroll").click(function(){
+		goto("#page-1");
+	});
+	
+
 	/*vTicker*/
  	$(function() {
 		$('#notice').vTicker();
-	});	
-  AOS.init();
+	});
+
+	/*AOS*/
+	AOS.init({ 
+	  offset: 200, 
+	  delay: 0,
+	  duration: 800, 
+	  easing: 'ease-in-out', 
+	  once: true, 
+	  mirror: false,
+	});
 	
 	/*swiper*/
 	var eventSwiper = new Swiper('#event-swiper', {
@@ -105,7 +109,6 @@ $(function() {
 	    prevEl: '#event-swiper-prev',
 	  }	  
 	})
-
 	var hotelSwiper = new Swiper('#hotel-swiper', {	  
 	  direction: 'horizontal',
 	  loop: true,	 
@@ -123,35 +126,35 @@ $(function() {
 
 	var reviewSwiperPic = new Swiper('#review-swiper', {	  
 	  direction: 'horizontal',
-	  loop: true,			  
+	  loop: true,	
+	  allowTouchMove: false		  
 	})
 	var reviewSwiperTxt = new Swiper('#review-swiper', {	  
 	  direction: 'horizontal',
 	  loop: true,	 
+	  allowTouchMove: false,
 	  navigation: {
 	    nextEl: '#review-swiper-next',
 	    prevEl: '#review-swiper-prev',
 	  }
 	})
-	  var next = document.querySelector("#review-swiper-next");
+	var next = document.querySelector("#review-swiper-next");
 	  next.onclick = function() {
 	    reviewSwiperPic.slideNext(1000);
 	  }
-	  var prev = document.querySelector("#review-swiper-prev");
+	var prev = document.querySelector("#review-swiper-prev");
 	  prev.onclick = function() {
 	    reviewSwiperPic.slidePrev(1000);
 	  }
-
-	goto();	
 	
 
- }); //window로드끝
+}); //window로드끝
 
 
 function goto(target) {
-	$("html, body, #main").animate({
-			scrollTop: $(target).offset().top
-		},1000);
+	$("html, body").animate({
+		scrollTop: $(target).offset().top
+	},1000);
 };
 
 function resize() {
@@ -159,90 +162,6 @@ function resize() {
 	wrapDiv.forEach(function(el){		
 		el.style.height = window.innerHeight + "px";	
 	});	
-};
-
-function nav() {
-	$("#to-top").click(function(){
-		goto("#top");
-		console.log(goto())
-	});
-	$("#scroll").click(function(){
-		goto("#page-1");
-	});
-};
-
-function showContents() {
-	if ($(window).scrollTop() >= 800) {			    
-		$("#intro-box-wrap").animate({				
-			 opacity: '1'
-		}, 600);		
-		$("#intro-box-wrap .intro-box").each(function(i) {		
-			$(this).animate({						
-				opacity: '1',
-				top: '0'
-			}, 800 * (i + 0.05));							
-		});
-		$("#page-2").animate({				
-			 	opacity: '1',
-				top: '0'
-		}, 600);					
-	
-	};
-	if ($(window).scrollTop() >= 1400) {
-		$("#page-3").animate({				
-		 	opacity: '1',
-			top: '0'
-		}, 800);		
-	};
-	if ($(window).scrollTop() >= 2300) {	
-		$("#page-4").animate({				
-		 	opacity: '1',
-			top: '0'
-		}, 800);	
-	};
-	if ($(window).scrollTop() >= 2700) {
-		$("#page-5").animate({				
-			 	opacity: '1',
-				top: '0'
-		}, 800);					
-	};
-
-	if ($(window).scrollTop() >= 3100) {
-		$("#page-6").animate({				
-			 	opacity: '1',
-				top: '0'
-		}, 800);		
-	};
-	if ($(window).scrollTop() >= 3900) {
-		$("#page-7").animate({				
-			 	opacity: '1',
-				top: '0'
-		}, 800);		
-		$("#page-8").animate({				
-			 	opacity: '1',
-				top: '0'
-		}, 800);		
-	};	//스위치문으로로 바꾸기..	
-
-};
-
-function scrollUpDown() { //스크롤업다운체크용
-	var oldScroll = 0;
-	$(window).scroll(function(){		
-		var nowScroll = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
-		if(nowScroll < oldScroll) {
-			$("#preloader").text("up");
-			$("#main-title").animate({
-				 height: window.innerHeight + 'px',
-			},800,easeInQuart);	
-		} else {
-			$("#preloader").text("down");
-			$("#main-title").animate({
-				 height: '0',
-			},800,easeInQuart);					
-		}
-		oldScroll = nowScroll <= 0 ? 0 : nowScroll;
-	});
 };
 
 function hoverMenu() {
@@ -264,7 +183,7 @@ function hoverMenu() {
 						setTimeout(function() {
 							$(".icon-bar").stop().animate({	
 								width: '130%'					
-							}, 600,'easeOutElastic');					
+							}, 300,'easeOutElastic');					
 						},400); clearTimeout();								
 				}, 300);					
 			})($(this));
@@ -355,7 +274,7 @@ function hoverBox() {
 			(function(el) {
 				timer[i] = setTimeout(function() {
 					el.css({
-					    boxShadow: '0px 0px 300px rgba(0,0,0,0.5) inset'
+					    boxShadow: '0px 0px 300px rgba(0,0,0,0.6) inset'
 					});
 					el.find(".pin").stop().animate({
 						right: '10px'
@@ -405,11 +324,39 @@ function hoverBox() {
 		});
 	});				
 };
-AOS.init({ 
-  offset: 200, 
-  delay: 0,
-  duration: 1000, 
-  easing: 'ease-in-out', 
-  once: true, 
-  mirror: false,
-});
+
+function BurgerNav() {
+	var mediaScreenMo = window.matchMedia( '( max-width: 500px )' );	
+	if ( mediaScreenMo.matches == true ) {
+		$("#top-nav-wrap").css({
+			top: '-280px'
+		});	
+	}
+	 else {
+		$("#top-nav-wrap").css({
+			top: '-100px'
+		});	
+	};
+
+	$("#burger-wrap").click(function(){	
+		if ( mediaScreenMo.matches == true ) {			
+			$("#top-nav-wrap").animate({
+					top: '0'
+			},300,'easeOutQuart');
+			$("#nav-bar").mouseleave(function(){
+				$("#top-nav-wrap").stop().animate({
+						top: '-280px'
+				},300,'easeOutQuart');	
+			});
+		} else {			
+			$("#top-nav-wrap").animate({
+					top: '0'
+			},300,'easeOutQuart');
+			$("#nav-bar").mouseleave(function(){
+				$("#top-nav-wrap").stop().animate({
+						top: '-100px'
+				},300,'easeOutQuart');	
+			});
+		};		
+	});
+};
